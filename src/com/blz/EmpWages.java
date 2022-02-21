20e22c3f126fed30b8b01336aee4787628d908b8
Rohit <rohitkamble10792@gmail.com>
2022-02-21 23:45:42 +0530

    Added: Functionality to check Employee is presnt or absent

--- src/com/blz/EmpWages.java
+++ src/com/blz/EmpWages.java
@@ -4,6 +4,17 @@ public class EmpWages {
     public static void main(String[] args) {
 
         System.out.println("Welcome to EmpWages Program");
+
+        int empCheck = (int) Math.floor(Math.random() * 10) % 2;
+        System.out.println(empCheck);
+
+        if (empCheck == 1) {
+            System.out.println("Employee is Present");
+        }
+        else {
+            System.out.println("Employee is Absent");
+        }
+
     }
 }
 
