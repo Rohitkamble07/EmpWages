99133d1e8efe37db9006af2833f1a63cfab099f5
Rohit <rohitkamble10792@gmail.com>
2022-02-21 23:56:20 +0530

    Replaced: Hardcoded Value

--- src/com/blz/EmpWages.java
+++ src/com/blz/EmpWages.java
@@ -4,14 +4,13 @@ public class EmpWages {
     public static void main(String[] args) {
 
         System.out.println("Welcome to EmpWages Program");
-
+        int FULL_TIME = 1;
         int empCheck = (int) Math.floor(Math.random() * 10) % 2;
         System.out.println(empCheck);
 
-        if (empCheck == 1) {
+        if (empCheck == FULL_TIME) {
             System.out.println("Employee is Present");
-        }
-        else {
+        } else {
             System.out.println("Employee is Absent");
         }
 
